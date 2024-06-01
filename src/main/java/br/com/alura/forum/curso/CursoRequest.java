e0d238d8f5e43b560df67b4a9b6f5aa12368d28e
Versart <wesleyversart@gmail.com>
2024-06-01 01:39:46 -0300

    feat: Add Builder
    Adds Builder to CursoRequest and CursoResponse classes

--- src/main/java/br/com/alura/forum/curso/CursoRequest.java
+++ src/main/java/br/com/alura/forum/curso/CursoRequest.java
@@ -2,7 +2,9 @@ package br.com.alura.forum.curso;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record CursoRequest(
         @NotBlank
         @NotNull
--- src/main/java/br/com/alura/forum/curso/CursoResponse.java
+++ src/main/java/br/com/alura/forum/curso/CursoResponse.java
@@ -1,5 +1,8 @@
 package br.com.alura.forum.curso;
 
+import lombok.Builder;
+
+@Builder
 public record CursoResponse(Long id, String nome, String categoria) {
 
     public CursoResponse(Curso curso){
